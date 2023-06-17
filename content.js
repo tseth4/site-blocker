001a6c9ad6d21af61ea2ba44c6268e597fb96569
ts <tristansetha@gmail.com>
2023-06-17 14:26:54 -0700

    update

--- content.js
+++ content.js
@@ -1,14 +1,9 @@
 chrome.runtime.onMessage.addListener((request) => {
   let blockedHtmlUrl = chrome.runtime.getURL("blocked.html");
-  console.log("blockedHtmlUrl: ", blockedHtmlUrl)
   chrome.storage.local.get("blockedSites", (response) => {
-    console.log("response: ", response);
     response.blockedSites.forEach((e) => {
       if (e) {
-        console.log(e);
         let regex = new RegExp(e);
-        console.log(regex);
-        console.log(request.tabUrl);
         if (regex.test(request.tabUrl)) {
           console.log("not allowed");
           window.location.href = blockedHtmlUrl;
--- options.js
+++ options.js
@@ -1,38 +1,83 @@
-// need disabled button functionality if no input is present or incorrect format
-// need to maintain the input inside box.
-function saveSites() {
-  let sitesInput = document.getElementById("site-input").value;
-  let sitesArr = sitesInput.split(",");
-  chrome.storage.local.set({
-    blockedSites: sitesArr,
-  });
+
+
+function saveSite(site) {
+  chrome.storage.local.get("blockedSites", (res) => {
+    if (res.blockedSites) {
+      chrome.storage.local.set({ blockedSites: [...res.blockedSites, site] })
+    } else {
+      chrome.storage.local.set({ blockedSites: [site] })
+    }
+  })
 }
 
-document.querySelector("#save-sites").addEventListener("click", () => {
-  saveSites();
-});
+let saveSiteInput = document.querySelector(".save_site_input");
+let siteListElement = document.querySelector("#site-list");
+let saveSiteButton = document.querySelector(".save_site_button");
+
+saveSiteButton.addEventListener("click", () => {
+  if (saveSiteInput.value) {
+    console.log("hasvalue")
+    saveSite(saveSiteInput.value);
+  } else {
+    console.log("novalue");
+  }
+})
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
   let siteList = document.querySelector("#site-list");
   siteList.innerHTML = "";
-  changes.blockedSites.newValue.forEach((e) => {
-    let newNode = document.createElement("li");
-    newNode.innerHTML = e;
+  changes.blockedSites.newValue.forEach((e, i) => {
+    let newNode = createBlockedSiteNode(e, i);
     siteList.appendChild(newNode);
   });
+  handleRemoveSiteButtons()
 });
 
 chrome.storage.local.get("blockedSites", (response) => {
-  console.log(response)
   if (!isEmpty(response)) {
     let siteList = document.querySelector("#site-list");
-    response.blockedSites.forEach((e) => {
-      let newNode = document.createElement("li");
-      newNode.innerHTML = e;
+    response.blockedSites.forEach((e, i) => {
+      let newNode = createBlockedSiteNode(e, i);
       siteList.appendChild(newNode);
     });
   }
+  handleRemoveSiteButtons()
 });
+
+function handleRemoveSiteButtons() {
+  let removeSiteButtons = document.querySelectorAll(".site_list__remove_button")
+  for (let i = 0; i < removeSiteButtons.length; i++) {
+    removeSiteButtons[i].addEventListener("click", (e) => {
+      // console.log("e button: ", e);
+      // console.log("rsbi: ", removeSiteButtons[i]);
+      removeSiteFromLocalStorage(i)
+
+    })
+  }
+}
+
+function removeSiteFromLocalStorage(index){
+  chrome.storage.local.get("blockedSites", (response) => {
+    let newBlockedSites = response.blockedSites;
+    newBlockedSites.splice(index, 1);
+    chrome.storage.local.set({ blockedSites: newBlockedSites});
+  })
+}
+
+function createBlockedSiteNode(site, index) {
+  let newNode = document.createElement("div");
+  let newUrl = document.createElement("div");
+  let newRemoveButton = document.createElement("button");
+  newNode.className = "site_list__site";
+  newRemoveButton.innerHTML = "-"
+  newRemoveButton.dataset.id = index;
+  newRemoveButton.className = "site_list__remove_button";
+  newNode.dataset.id = index;
+  newUrl.innerHTML = site;
+  newNode.appendChild(newUrl);
+  newNode.appendChild(newRemoveButton);
+  return newNode;
+}
 
 function isEmpty(obj) {
   for (var prop in obj) {
